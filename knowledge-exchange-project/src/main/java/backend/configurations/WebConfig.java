3a50803b1dc61f3813d0b1956c530f4c56e3c358
mie378 <elmosiek@gmail.com>
2023-06-08 18:43:10 +0200

    switch jackson to gson

--- knowledge-exchange-project/src/main/java/backend/configurations/WebConfig.java
+++ knowledge-exchange-project/src/main/java/backend/configurations/WebConfig.java
@@ -1,6 +1,9 @@
 package backend.configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
