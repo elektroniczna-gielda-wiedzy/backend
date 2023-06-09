59c0689e39ad23eacb790bb5d9249618398dbd94
Mateusz Nowak <matteuszn65@gmail.com>
2023-06-09 20:04:50 +0200

    Added more example entries

--- knowledge-exchange-project/src/main/resources/data.sql
+++ knowledge-exchange-project/src/main/resources/data.sql
@@ -88,8 +88,14 @@ INSERT INTO users(first_name, last_name, email, password, is_email_auth, created
 
 INSERT INTO entry(user_id, entry_type_id, title, content, created_at, updated_at, is_deleted) VALUES
 (1, 2, 'Tytuł ogłoszenia', 'Content ogłoszenia', current_timestamp - INTERVAL '10 minutes', current_timestamp, false),
-(1, 1, 'Tytuł notatki', 'Content notatki', current_timestamp, current_timestamp, false),
-(1, 3, 'Tytuł postu', 'Content postu', current_timestamp - INTERVAL '20 minutes', current_timestamp, false);
+(1, 1, 'Tytuł notatki z obrazem', 'Content notatki', current_timestamp, current_timestamp, false),
+(1, 3, 'Tytuł postu z obrazem', 'Content postu', current_timestamp - INTERVAL '20 minutes', current_timestamp, false),
+(2, 1, 'Tytuł notatki', 'Content notatki', current_timestamp - INTERVAL '30 minutes', current_timestamp, false),
+(2, 2, 'Tytuł ogłoszenia', 'Content ogłoszenia', current_timestamp - INTERVAL '40 days', current_timestamp, false),
+(2, 3, 'Tytuł postu', 'Content postu', current_timestamp - INTERVAL '50 days', current_timestamp, false),
+(3, 1, 'Tytuł notatki', 'Content notatki', current_timestamp - INTERVAL '60 days', current_timestamp, false),
+(3, 2, 'Tytuł ogłoszenia', 'Content ogłoszenia', current_timestamp - INTERVAL '70 days', current_timestamp, false),
+(3, 3, 'Tytuł postu', 'Content postu', current_timestamp - INTERVAL '80 days', current_timestamp, false);
 
 INSERT INTO entry_category(entry_id, category_id) VALUES (1, 1),
                                                          (1, 4),
@@ -101,7 +107,29 @@ INSERT INTO entry_category(entry_id, category_id) VALUES (1, 1),
                                                          (3, 2),
                                                          (3, 10),
                                                          (3, 14),
-                                                         (3, 22);
+                                                         (3, 22),
+                                                            (4, 1),
+                                                            (4, 4),
+                                                            (4, 14),
+                                                            (4, 20),
+                                                            (5, 3),
+                                                            (5, 12),
+                                                            (5, 15),
+                                                            (6, 2),
+                                                            (6, 10),
+                                                            (6, 14),
+                                                            (6, 22),
+                                                            (7, 1),
+                                                            (7, 4),
+                                                            (7, 14),
+                                                            (7, 20),
+                                                            (8, 3),
+                                                            (8, 12),
+                                                            (8, 15),
+                                                            (9, 2),
+                                                            (9, 10),
+                                                            (9, 14),
+                                                            (9, 22);
 
 INSERT INTO image(image) VALUES
 ('http://localhost:8080/image1.jpg'),
