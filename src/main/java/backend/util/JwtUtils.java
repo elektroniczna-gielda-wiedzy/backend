95f960d24387ec6b943cc6c0971e5e681cf9cf27
mie378 <elmosiek@gmail.com>
2023-06-25 03:31:49 +0200

    jwt tokens work after server restart

--- src/main/java/backend/util/JwtUtils.java
+++ src/main/java/backend/util/JwtUtils.java
@@ -3,8 +3,15 @@ package backend.util;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 
 public class JwtUtils {
-    public static Key jwtKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    public static final String secretValue = "valueeeevalueeeevalueeeevalueeee";
+    public static Key jwtKey = generateKey();
+
+    public static Key generateKey() {
+        return new SecretKeySpec(secretValue.getBytes(), "HmacSHA512");
+    }
 }
