1e3fe3f6d58b9b532e4a1f1019f960d368a5e0c2
Mateusz Nowak <matteuszn65@gmail.com>
2023-06-28 17:20:02 +0200

    Added more real like examples

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -85,7 +85,7 @@ INSERT INTO users(first_name, last_name, email, password, is_email_auth, created
                                                                                                                           ('Marek', 'Król', 'marekkrol@student.agh.edu.pl', '$2a$10$mraQzwvvxI0GK.u.IvAwV.esajUJb5d2l.qpfNWIAx.kwu6Yy44QK', true, current_timestamp, current_timestamp, false, true),
                                                                                                                           ('Patryk', 'Kowal', 'patrykkowal@student.agh.edu.pl', '$2a$10$mraQzwvvxI0GK.u.IvAwV.esajUJb5d2l.qpfNWIAx.kwu6Yy44QK', true, current_timestamp, current_timestamp, false, true),
                                                                                                                           ('Michał', 'Markowski', 'michalmarkowski@student.agh.edu.pl', '$2a$10$mraQzwvvxI0GK.u.IvAwV.esajUJb5d2l.qpfNWIAx.kwu6Yy44QK', true, current_timestamp, current_timestamp, false, true),
-                                                                                                                          ('Adrian', 'Król', 'adriankrol3@student.agh.edu.pl', '$2a$10$mraQzwvvxI0GK.u.IvAwV.esajUJb5d2l.qpfNWIAx.kwu6Yy44QK', true, current_timestamp, current_timestamp, false, true),
+                                                                                                                          ('Adrian', 'Król', 'adriankrol@student.agh.edu.pl', '$2a$10$mraQzwvvxI0GK.u.IvAwV.esajUJb5d2l.qpfNWIAx.kwu6Yy44QK', true, current_timestamp, current_timestamp, false, true),
                                                                                                                           ('Patryk', 'Markowski', 'patrykmarkowski@student.agh.edu.pl', '$2a$10$mraQzwvvxI0GK.u.IvAwV.esajUJb5d2l.qpfNWIAx.kwu6Yy44QK', true, current_timestamp, current_timestamp, false, true),
                                                                                                                           ('Admin', 'Adminowski', 'admin@student.agh.edu.pl', '$2a$10$mraQzwvvxI0GK.u.IvAwV.esajUJb5d2l.qpfNWIAx.kwu6Yy44QK', true, current_timestamp, current_timestamp, true, true);
 
@@ -106,7 +106,7 @@ Plik znajduję się na moim dysku google, poniżej wklejam link do niego
 https://drive.google.com/file/d/1NPnrH_UrqTQdgfdFOIJ05DGDEC5sdfYPsNU/view?usp=sharing
 
 Na zamieszczonym zdjęciu znajduje się pierwsza strona notatek',
-                                                                                                             current_timestamp - INTERVAL '160 minutes', current_timestamp, false),
+                                                                                                             current_timestamp, current_timestamp, false),
 
                                                                                                             (2, 4, 2, 'Korepetycje z analizy matematycznej', 'Cześć, jestem studentem czwartego roku matematyki. Posiadam dobre zrozumienie analizy matematycznej i oferuję pomoc w zrozumieniu trudnych koncepcji oraz przygotowaniu do egzaminu. Materiały z lekcji mogę dostarczyć w formie elektronicznej. Zajęcia mogą odbywać się online lub osobiście, w zależności od Twojej preferencji.
 
@@ -121,14 +121,14 @@ Sesje mogą obejmować następujące tematy:
 Stawka to 30 zł / godzina.
 
 Chętnych do nauki proszę o nawiązanie kontaktu poprzez system czatowy w aplikacji.',
-                                                                                                             current_timestamp - INTERVAL '70 minutes', current_timestamp, false),
+                                                                                                             current_timestamp, current_timestamp, false),
                                                                                                             (3, 4, 3, 'Problem z nieskończoną pętlą w algorytmie Bubble Sort', 'Cześć wszystkim, mam problem z implementacją algorytmu sortowania bąbelkowego w Pythonie. Poniżej znajduje się kod, który napisałem, ale niestety program wpada w nieskończoną pętlę. Czy mógłby mi ktoś pomóc?',
-                                                                                                             current_timestamp - INTERVAL '90 minutes', current_timestamp, false),
+                                                                                                             current_timestamp, current_timestamp, false),
                                                                                                             (4, 1, 1, 'Najlepsze praktyki z zakresu cyberbezpieczeństwa', 'Ta notatka omówi niektóre z najważniejszych zasad cyberbezpieczeństwa, które mogą być użyte do ochrony Twoich informacji. Dowiesz się jak stosować najlepsze praktyki z zakresu haseł, bezpiecznych nawyków przeglądania, dwuskładnikowego uwierzytelniania, i więcej. Zajrzyj pod poniższy link' ||
                                                                                                                                                                           '
 
                                                                                                                                                                           https://drive.google.com/file/d/1NPnrH_UrqTQdgfdFOIJ05DGDEC5sdfYPsNU/view?usp=sharing', current_timestamp - INTERVAL '160 minutes', current_timestamp, false),
-                                                                                                            (5, 1, 3, 'Badanie wpływu kultury w erze cyfrowej post z obrazem', 'Ten post omówi sposoby, w jakie technologia cyfrowa, internet i media społecznościowe wpływają na kulturę, w tym na sposób, w jaki komunikujemy się, jak postrzegamy siebie i jak odnosimy się do innych.', current_timestamp, current_timestamp, false),
+                                                                                                            (5, 1, 3, 'Badanie wpływu kultury w erze cyfrowej', 'Piszę pracę o wpływie internetu i technologi cyfrowej na kulturę. Czy moglibyście podzielić się swoimi odczuciami i przemyśleniami odnoścnie tego czy zaważanie wpływ internetu na to w jaki sposób komunikujecie się i odnosicie do innych ludzi?', current_timestamp - INTERVAL '90 minutes', current_timestamp, false),
                                                                                                             (6, 1, 1, 'Piękno matematyki dyskretnej notatka z obrazem', 'Głębokie zanurzenie w piękny świat matematyki dyskretnej. W tej notatce wyjaśniam, jak matematyka dyskretna pomaga w rozwiązywaniu skomplikowanych problemów informatyki.', current_timestamp - INTERVAL '20 minutes', current_timestamp, false),
                                                                                                             (7, 2, 2, 'Pomoc w zadaniach domowych z algebry', 'Jeśli ktokolwiek ma problemy z zadaniem domowym z algebry, śmiało pisz. Mogę pomóc w tematach obejmujących równania liniowe, nierówności, wykresy, macierze, wielomiany i wyrażenia pierwiastkowe, równania i funkcje kwadratowe, wyrażenia wykładnicze i logarytmiczne, sekwencje i serie, prawdopodobieństwo i więcej.', current_timestamp - INTERVAL '30 minutes', current_timestamp, false),
                                                                                                             (8, 3, 1, 'Zrozumienie pochodnych w analizie matematycznej', 'W tej notatce wyjaśniłem pojęcie pochodnych w analizie matematycznej. Notatka zaczyna się od podstawowej definicji, po której następują reguły różniczkowania i zastosowania. Znajdują się tam przykłady i problemy do samodzielnego rozwiązania.' ||
@@ -138,16 +138,16 @@ Chętnych do nauki proszę o nawiązanie kontaktu poprzez system czatowy w aplik
                                                                                                             (10, 3, 3, 'Problem z programowaniem obiektowym', 'Mam problem ze zrozumieniem koncepcji dziedziczenia i polimorfizmu w programowaniu obiektowym. Czy ktoś mógłby to wyjaśnić za pomocą przykładu? Używam C++ na moim kursie.', current_timestamp - INTERVAL '80 minutes', current_timestamp, false),
                                                                                                             (11, 3, 2, 'Szukam partnera do nauki struktur danych', 'Szukam partnera do nauki na nadchodzący kurs struktur danych. Moim celem jest wspólne studiowanie i zdobycie 5.0 na tym kursie. Jeśli jesteś zainteresowany, odpisz na ten post.', current_timestamp - INTERVAL '90 minutes', current_timestamp, false),
 
-                                                                                                            (12, 1, 2, 'Tytuł ogłoszenia', 'Content ogłoszenia', current_timestamp - INTERVAL '10 minutes', current_timestamp, false),
-                                                                                                            (13, 1, 1, 'Tytuł notatki', 'Content notatki', current_timestamp, current_timestamp, false),
-                                                                                                            (14, 1, 3, 'Tytuł postu', 'Content postu', current_timestamp - INTERVAL '20 minutes', current_timestamp, false),
-                                                                                                            (15, 2, 1, 'Tytuł notatki', 'Content notatki', current_timestamp - INTERVAL '30 minutes', current_timestamp, false),
-                                                                                                            (16, 2, 2, 'Tytuł ogłoszenia', 'Content ogłoszenia', current_timestamp - INTERVAL '3 days', current_timestamp, false),
-                                                                                                            (17, 2, 3, 'Tytuł postu', 'Content postu', current_timestamp - INTERVAL '7 days', current_timestamp, false),
-                                                                                                            (18, 3, 1, 'Tytuł notatki', 'Content notatki', current_timestamp - INTERVAL '8 days', current_timestamp, false),
-                                                                                                            (19, 3, 2, 'Tytuł ogłoszenia', 'Content ogłoszenia', current_timestamp - INTERVAL '70 days', current_timestamp, false),
-                                                                                                            (20, 3, 3, 'Tytuł postu', 'Content postu', current_timestamp - INTERVAL '200 days', current_timestamp, false),
-                                                                                                            (21, 3, 3, 'Tytuł postu', 'Content postu', current_timestamp - INTERVAL '200 days', current_timestamp, false);
+                                                                                                            (12, 4, 1, 'Wprowadzenie do fizyki kwantowej', 'Ta notatka obejmuje podstawy fizyki kwantowej, ważnego obszaru fizyki, który stanowi podstawę wielu nowoczesnych technologii. Zawiera szczegółowe wyjaśnienia, diagramy i przykładowe problemy. Znajdź notatkę tutaj: https://drive.google.com/file/d/1NPnrH_UrqTQdgfdFOIJ05DGDEC5sdfYPsNU/view?usp=sharing', current_timestamp - INTERVAL '160 hours', current_timestamp, false),
+                                                                                                            (13, 4, 3, 'Problem z pętlą w Pythonie', 'Cześć wszystkim, mam problem z programem w Pythonie. Wydaje się, że wpada on w nieskończoną pętlę. Czy ktoś może pomóc?', current_timestamp - INTERVAL '90 minutes', current_timestamp, false),
+                                                                                                            (14, 5, 1, 'Schemat bazy danych z ćwiczeń podczas eRajdu', 'Jako, że spora część osób była w piątek na eRajdzie dr Kowalski prosił, aby podzielić się z nieobecnymi schematem bazy nad którym pracowaliśmy. Jeśli coś jest niejasne możesz śmiało do mnie pisać, postaram się to wytłumaczyć najlepiej jak mogę', current_timestamp - INTERVAL '6 minutes', current_timestamp, false),
+                                                                                                            (15, 5, 3, 'Problem z rozwiązaniem zadania z programowania', 'Cześć wszystkim, mam problem z jednym z zadań z programowania. Chodzi o implementację algorytmu sortowania w języku Python. Czy ktoś mógłby mi pomóc zrozumieć, jak to zrobić?', current_timestamp - INTERVAL '300 minutes', current_timestamp, false),
+
+                                                                                                            (16, 6, 2, 'Korepetycje z programowania w Pythonie', 'Cześć, jestem doświadczonym programistą Pythona i oferuję korepetycje dla osób na wszystkich poziomach zaawansowania. Możemy pracować nad podstawami, strukturami danych, algorytmami, a nawet zaawansowanymi tematami takimi jak uczenie maszynowe. Skontaktuj się ze mną przez system czatu.', current_timestamp - INTERVAL '930 minutes', current_timestamp, false),
+
+                                                                                                            (17, 6, 2, 'Szukam grupy do nauki termodynamiki', 'Cześć, próbuję zorganizować grupę do nauki na nadchodzący egzamin z termodynamiki. Jeśli jesteś zainteresowany, wyślij mi wiadomość. Planujemy spotykać się dwa razy w tygodniu i pracować nad arkuszami egzaminacyjnymi i trudnymi pytania.', current_timestamp - INTERVAL '50 days', current_timestamp, false),
+                                                                                                            (18, 7, 3, 'Pytanie o prowadzących kurs analizy matematycznej', 'Cześć, czy ktoś z was może mi powiedzieć, jakie są godziny konsultacji u prowadzących kurs analizy matematycznej? Z góry dzięki za pomoc!', current_timestamp - INTERVAL '12 minutes', current_timestamp, false);
+
 
 INSERT INTO entry_category(entry_id, category_id) VALUES
                                                          (1, 15),
@@ -201,8 +201,7 @@ INSERT INTO entry_category(entry_id, category_id) VALUES
                                                         (12, 1),
                                                         (12, 15),
                                                         (12, 6),
-                                                        (12, 16),
-                                                        (12, 17),
+                                                        (12, 23),
 
                                                         (13, 1),
                                                         (13, 6),
@@ -212,42 +211,27 @@ INSERT INTO entry_category(entry_id, category_id) VALUES
                                                         (14, 1),
                                                         (14, 6),
                                                         (14, 13),
-                                                        (14, 16),
+                                                        (14, 17),
 
                                                         (15, 1),
                                                         (15, 6),
                                                         (15, 13),
                                                         (15, 16),
 
-                                                        (16, 1),
-                                                        (16, 6),
-                                                        (16, 13),
+                                                        (16, 17),
                                                         (16, 16),
+                                                        (16, 18),
+                                                        (16, 13),
 
                                                         (17, 1),
-                                                        (17, 6),
+                                                        (17, 7),
                                                         (17, 13),
                                                         (17, 16),
 
                                                         (18, 1),
-                                                        (18, 7),
-                                                        (18, 13),
-                                                        (18, 16),
-
-                                                        (19, 1),
-                                                        (19, 7),
-                                                        (19, 13),
-                                                        (19, 16),
-
-                                                        (20, 2),
-                                                        (20, 9),
-                                                        (20, 13),
-                                                        (20, 16),
-
-                                                        (21, 2),
-                                                        (21, 9),
-                                                        (21, 13),
-                                                        (21, 16);
+                                                        (18, 6),
+                                                        (18, 14),
+                                                        (18, 21);
 
 
 
@@ -259,7 +243,8 @@ Hej, może powinieneś spróbować użyć algorytmu sortowania przez wstawianie.
 Problemem jest logika Twojego kodu. W rzeczywistości, kiedy sortowanie bąbelkowe zaczyna porównywanie elementów, powinno początkowo założyć, że lista jest posortowana. Jeśli znajdzie jakiekolwiek dwa elementy, które są w złej kolejności, powinno ustawić flagę informującą, że lista jest niesortowana. Ta flaga pozostanie prawdziwa tylko wtedy, gdy wszystkie elementy są w prawidłowej kolejności.
 
 Dodatkowo warte wspomnienia jest to, że python umożliwia prostrzą zamianę wartości zmiennych bez tworzenia zmiennej tymczasowej',
-                                                                                                   current_timestamp - INTERVAL '60 minutes', false, true);
+                                                                                                   current_timestamp - INTERVAL '60 minutes', false, true),
+                                                                                                  (49, 13, 1, 'Czy mógłbyś podać kod, którego dotyczy problem?', current_timestamp - INTERVAL '5 minutes', false, false);
 
 
 INSERT INTO image(image) VALUES
@@ -267,7 +252,8 @@ INSERT INTO image(image) VALUES
                              ('image2.jpg'),
                              ('image3.jpg'),
                              ('image4.jpg'),
-                             ('image5.jpg');
+                             ('image5.jpg'),
+                             ('image6.png');
 
 
 INSERT INTO favorites(user_id, entry_id) VALUES (1, 1), (1, 2);
@@ -276,7 +262,8 @@ INSERT INTO image_entry(image_item_id, image_id) VALUES
                                                     (5, 1),
                                                     (6, 2),
                                                     (3, 3),
-                                                    (1, 4);
+                                                    (1, 4),
+                                                    (14, 6);
 INSERT INTO image_answer(image_item_id, image_id) VALUES
                                                     (50, 5);
 INSERT INTO chat(user_one_last_read, user_two_last_read, user_one_id, user_two_id) VALUES
