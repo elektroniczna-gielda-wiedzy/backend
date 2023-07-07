88eb77c3b830482eae7042c13bf00906bcfaf99d
mie378 <elmosiek@gmail.com>
2023-07-07 22:38:23 +0200

    add key for email confirmation jwt creation

--- src/main/java/backend/util/JwtUtils.java
+++ src/main/java/backend/util/JwtUtils.java
@@ -10,8 +10,13 @@ import java.security.Key;
 public class JwtUtils {
     public static final String secretValue = "valueeeevalueeeevalueeeevalueeee";
     public static Key jwtKey = generateKey();
+    public static final String secretConfirmationValue = "valueee2valueee2valueee2valueee2";
 
     public static Key generateKey() {
         return new SecretKeySpec(secretValue.getBytes(), "HmacSHA512");
+    }
+
+    public static Key generateEmailConfirmationKey() {
+        return new SecretKeySpec(secretConfirmationValue.getBytes(), "HmacSHA512");
     }
 }
