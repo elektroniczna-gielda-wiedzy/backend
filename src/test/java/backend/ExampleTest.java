ff45793326c3522872ec3380c6327f6d242332f7
Mateusz Furga <matfurga@gmail.com>
2023-09-19 13:20:38 +0200

    Add CI tests

---
+++ src/test/java/backend/ExampleTest.java
@@ -0,0 +1,20 @@
+package backend;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ExampleTest {
+    @Test
+    public void test1() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(1, 1);
+    }
+}
