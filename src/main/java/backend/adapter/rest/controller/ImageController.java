56a83255bb7125b11293b8ef65a6b800b1eb4f55
Mateusz Nowak <matteuszn65@gmail.com>
2023-10-15 16:20:30 +0200

    Reverted adding api/v1 to ImageController

--- src/main/java/backend/adapter/rest/controller/ImageController.java
+++ src/main/java/backend/adapter/rest/controller/ImageController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/image")
+@RequestMapping("image")
 public class ImageController {
     private final ImageService imageService;
 
