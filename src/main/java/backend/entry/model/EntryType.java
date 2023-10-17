2ae69dc4cb387ec943b63203ad9bf15f7f9a7944
Mateusz Furga <matfurga@gmail.com>
2023-10-17 13:48:10 +0200

    Fix EntryType

--- src/main/java/backend/entry/model/EntryType.java
+++ src/main/java/backend/entry/model/EntryType.java
@@ -32,24 +32,3 @@ public enum EntryType {
     }
 }
 
-
-//@Entity
-//@Table(name = "EntryType")
-//@Getter
-//@Setter
-//public class EntryType {
-//
-//    public static String ANNOUNCEMENT = "Announcement";
-//
-//    public static String NOTE = "Note";
-//
-//    public static String POST = "Post";
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "entry_type_id")
-//    private Integer id;
-//
-//    @Column(name = "name")
-//    private String name;
-//}
