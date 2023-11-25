a5f8f2bb6244551d05f42f604a5b838acad31b1e
mie378 <elmosiek@gmail.com>
2023-11-25 03:08:32 +0100

    add json body to 401

--- src/main/java/backend/adapter/rest/config/SecurityConfig.java
+++ src/main/java/backend/adapter/rest/config/SecurityConfig.java
@@ -1,7 +1,14 @@
 package backend.adapter.rest.config;
 
+import backend.adapter.rest.Response;
+import backend.adapter.rest.StandardBody;
 import backend.adapter.rest.security.JwtFilter;
 import backend.user.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -9,11 +16,16 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import java.io.IOException;
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
@@ -30,7 +42,16 @@ public class SecurityConfig {
                 .permitAll()
                 .anyRequest()
                 .authenticated()).exceptionHandling().
-                    authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+                    authenticationEntryPoint((request, response, authException) -> {
+                        response.setContentType("application/json");
+                        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                        response.getOutputStream().println(new ObjectMapper().writeValueAsString(StandardBody.builder()
+                                                                                     .success(false)
+                                                                                     .messages(List.of("Invalid or missing " +
+                                                                                                               "authentication token"))
+                                                                                     .result(List.of())
+                                                                                     .build()));
+                    });
         http.csrf().disable();
         http.addFilterBefore(new JwtFilter(userRepository), AuthorizationFilter.class);
         http.cors().and().authorizeHttpRequests();
